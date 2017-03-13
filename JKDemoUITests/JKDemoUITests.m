7e60c7299469c349eccb67d1e3a85968341e4b19
jackyjiao <jackyjiao@tencent.com>
2017-03-13 19:52:47 +0800

    直接调用私有APIs来执行UI自动化

--- JKDemoUITests/JKDemoUITests.m
+++ JKDemoUITests/JKDemoUITests.m
@@ -8,6 +8,9 @@
 
 #import <XCTest/XCTest.h>
 
+//import private APIs
+#import "PrivateHeaders/XCTest/XCUIApplication.h"
+
 @interface JKDemoUITests : XCTestCase
 
 @end
@@ -22,7 +25,7 @@
     // In UI tests it is usually best to stop immediately when a failure occurs.
     self.continueAfterFailure = NO;
     // UI tests must launch the application that they test. Doing this in setup will make sure it happens for each test method.
-    [[[XCUIApplication alloc] init] launch];
+    // [[[XCUIApplication alloc] init] launch];
 
     // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
 }
@@ -35,12 +38,14 @@
 - (void)testExample {
     // Use recording to get started writing UI tests.
     // Use XCTAssert and related functions to verify your tests produce the correct results.
-    XCUIApplication *app = [[XCUIApplication alloc] init];
+    XCUIApplication *app;
+    app = [[XCUIApplication alloc] initPrivateWithPath:nil bundleID:@"com.tencent.imsdk.JKDemo"];
+    [app launch];
     [app.tables.staticTexts[@"动画"] tap];
     [app.navigationBars[@"动画"].buttons[@"工具"] tap];
     [app.tables.staticTexts[@"退出登录"] tap];
 
-    app = [[XCUIApplication alloc] init];
+    app = [[XCUIApplication alloc] initPrivateWithPath:nil bundleID:@"com.tencent.imsdk.JKDemo"];
     XCUIElement *textField = app.textFields[@"账号"];
     [textField tap];
     [textField typeText:@"123"];
@@ -50,6 +55,22 @@
     [textField typeText:@"asd"];
 
     [app.buttons[@"Login"] tap];
+    
+    //QQ
+    app = [[XCUIApplication alloc] initPrivateWithPath:nil bundleID:@"com.tencent.mqq"];
+    [app launch];
+    
+    textField = app.textFields[@"QQ号, 或 手机号, 或 邮箱"];
+    [textField tap];
+    [textField typeText:@"123"];
+    
+    textField = app.secureTextFields[@"密码"];
+    [textField tap];
+    [textField typeText:@"asdfghjkhkj"];
+    [app.buttons[@"登录按钮"] tap];
+    
+    [app terminate];
+
     
 }
 
