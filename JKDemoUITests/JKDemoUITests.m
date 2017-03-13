16baa1f38c91742d5465758ee16bd25704e25c12
jackyjiao <jackyjiao@tencent.com>
2017-03-13 19:09:46 +0800

    add WebDriverAgent Framework

--- JKDemoUITests/JKDemoUITests.m
+++ JKDemoUITests/JKDemoUITests.m
@@ -8,6 +8,8 @@
 
 #import <XCTest/XCTest.h>
 
+#import <WebDriverAgentLib/FBApplication.h>
+
 @interface JKDemoUITests : XCTestCase
 
 @end
@@ -22,7 +24,7 @@
     // In UI tests it is usually best to stop immediately when a failure occurs.
     self.continueAfterFailure = NO;
     // UI tests must launch the application that they test. Doing this in setup will make sure it happens for each test method.
-    [[[XCUIApplication alloc] init] launch];
+    // [[[XCUIApplication alloc] init] launch];
 
     // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
 }
@@ -35,21 +37,62 @@
 - (void)testExample {
     // Use recording to get started writing UI tests.
     // Use XCTAssert and related functions to verify your tests produce the correct results.
-    XCUIApplication *app = [[XCUIApplication alloc] init];
+    FBApplication *app;
+    XCUIElement *textField;
+    app = [[FBApplication alloc] initPrivateWithPath:nil bundleID:@"com.tencent.imsdk.JKDemo"];
+    app.fb_shouldWaitForQuiescence = YES;
+    
+    [app launch];
     [app.tables.staticTexts[@"动画"] tap];
     [app.navigationBars[@"动画"].buttons[@"工具"] tap];
+    
     [app.tables.staticTexts[@"退出登录"] tap];
-
-    app = [[XCUIApplication alloc] init];
-    XCUIElement *textField = app.textFields[@"账号"];
+    
+    app = [[FBApplication alloc] initPrivateWithPath:nil bundleID:@"com.tencent.imsdk.JKDemo"];
+    app.fb_shouldWaitForQuiescence = YES;
+    textField = app.textFields[@"账号"];
     [textField tap];
     [textField typeText:@"123"];
     
     textField = app.secureTextFields[@"密码"];
     [textField tap];
     [textField typeText:@"asd"];
-
+    
     [app.buttons[@"Login"] tap];
+    [app terminate];
+    
+    app = [[FBApplication alloc] initPrivateWithPath:nil bundleID:@"com.tencent.mqq"];
+    app.fb_shouldWaitForQuiescence = YES;
+    
+    [app launch];
+    
+    textField = app.textFields[@"QQ号, 或 手机号, 或 邮箱"];
+    [textField tap];
+    [textField typeText:@"123"];
+    
+    textField = app.secureTextFields[@"密码"];
+    [textField tap];
+    [textField typeText:@"asdfghjkhkj"];
+    [app.buttons[@"登录按钮"] tap];
+    
+    [app terminate];
+    
+    
+    
+    app = [[FBApplication alloc] initPrivateWithPath:nil bundleID:@"com.netmarble.penta"];
+    app.fb_shouldWaitForQuiescence = YES;
+    
+    [app launch];
+    
+    [app.buttons[@"Facebook"] tap];
+    
+    [app.tables.staticTexts[@"登录"] tap];
+    
+    app = [FBApplication fb_activeApplication];
+    [app.buttons[@"确定"] tap];
+    
+    app = [FBApplication fb_activeApplication];
+    [app.buttons[@"GameCenter"] tap];
     
 }
 
